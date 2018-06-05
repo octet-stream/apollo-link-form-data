1466c389efe8884ed56d6564e2bd3e6f9d8e59d0
Nick K <nick.kruchinin@gmail.com>
2018-06-06 02:51:41 +0300

    One more test!

--- test/link.js
+++ test/link.js
@@ -7,6 +7,8 @@ const gql = require("graphql-tag")
 
 const {createFormDataLink} = require("..")
 
+const uri = "http://localhost:2319/graphql"
+
 test.beforeEach(() => void mock.restore())
 
 test("Should create a link without any argument", t => {
@@ -18,7 +20,9 @@ test("Should create a link without any argument", t => {
 })
 
 test("Should always make a request with POST method", async t => {
-  mock.post("http://localhost:2319/graphql", {
+  t.plan(1)
+
+  mock.post(uri, {
     data: {
       noop: null
     }
@@ -30,14 +34,52 @@ test("Should always make a request with POST method", async t => {
     }
   `
 
-  const link = createFormDataLink({
-    uri: "http://localhost:2319/graphql",
-    method: "GET"
-  })
+  const link = createFormDataLink({uri, method: "GET"})
 
   await makePromise(execute(link, {query}))
 
   const [, {method}] = mock.lastCall()
 
-  t.is(String(method).toLowerCase(), "post")
+  t.is(
+    String(method).toLowerCase(), "post",
+    "The POST method must be used for every request " +
+    "even if different method was specified."
+  )
 })
+
+test(
+  "Should send a request with JSON body unless files were defined in variables",
+  async t => {
+    t.plan(4)
+
+    mock.post(uri, {
+      data: {
+        doNothing: null
+      }
+    })
+
+    const query = gql`
+      mutation DoNothing($noop: String!) {
+        doNothing(noop: $noop)
+      }
+    `
+
+    const variables = {
+      noop: "noop"
+    }
+
+    const link = createFormDataLink({uri})
+
+    await makePromise(execute(link, {query, variables}))
+
+    const [, {headers, body}] = mock.lastCall()
+
+    // Check headers
+    t.true("content-type" in headers)
+    t.is(String(headers["content-type"]).toLowerCase(), "application/json")
+
+    // Check body
+    t.true(typeof body === "string")
+    t.is(JSON.parse(body).variables.noop, "noop")
+  }
+)
