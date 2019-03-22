f3fb4cfe3d78776bb53dfcfe641b087b69a370da
Nick K <nick.kruchinin@gmail.com>
2019-03-23 02:53:53 +0300

    Improve makeFetcherPayload: Add content-length header for FormData body whrn available.

--- test/util/makeFetcherPayload.js
+++ test/util/makeFetcherPayload.js
@@ -26,21 +26,24 @@ function isJSONValid(string) {
   }
 }
 
-test("Returns a JSON payload request when request have no variables", t => {
-  const expected = {
-    body: JSON.stringify(paramsTemplate.body),
-    headers: {
-      "content-type": "application/json"
+test(
+  "Returns a JSON payload request when request have no variables",
+  async t => {
+    const expected = {
+      body: JSON.stringify(paramsTemplate.body),
+      headers: {
+        "content-type": "application/json"
+      }
     }
+
+    const actual = await makePayload(paramsTemplate)
+
+    t.deepEqual(expected, actual)
+    t.true(isJSONValid(actual.body))
   }
+)
 
-  const actual = makePayload(paramsTemplate)
-
-  t.deepEqual(expected, actual)
-  t.true(isJSONValid(actual.body))
-})
-
-test("Returns a JSON payload when body does not contain files", t => {
+test("Returns a JSON payload when body does not contain files", async t => {
   const params = {
     ...paramsTemplate,
 
@@ -53,12 +56,12 @@ test("Returns a JSON payload when body does not contain files", t => {
     }
   }
 
-  const actual = makePayload(params)
+  const actual = await makePayload(params)
 
   t.true(isJSONValid(actual.body))
 })
 
-test("Returns form-data payload whrn body have at least one file", t => {
+test("Returns form-data payload whrn body have at least one file", async t => {
   const fd = new FormData()
   const mockedMakePayload = pq("../../lib/util/makeFetcherPayload", {
     "@octetstream/object-to-form-data": () => fd
@@ -76,67 +79,94 @@ test("Returns form-data payload whrn body have at least one file", t => {
     }
   }
 
-  const actual = mockedMakePayload(params)
+  const expected = {
+    ...fd.headers,
+
+    "content-length": await fd.getComputedLength(),
+    accept: "*/*"
+  }
+
+  const actual = await mockedMakePayload(params)
 
   t.true(actual.body instanceof Readable)
   t.is(fd.stream, actual.body)
-  t.deepEqual({...fd.headers, accept: "*/*"}, actual.headers)
+  t.deepEqual(expected, actual.headers)
 
   t.pass()
 })
 
-test("Always returns form-data payload when force mode is set", t => {
-  const fd = new FormData()
-  const mockedMakePayload = pq("../../lib/util/makeFetcherPayload", {
-    "@octetstream/object-to-form-data": () => fd
-  })
+test(
+  "Always returns form-data payload when force mode is set",
+  async t => {
+    const fd = new FormData()
+    const mockedMakePayload = pq("../../lib/util/makeFetcherPayload", {
+      "@octetstream/object-to-form-data": () => fd
+    })
 
-  const params = {
-    ...paramsTemplate,
+    const params = {
+      ...paramsTemplate,
 
-    mode: {
-      force: true
-    },
-    body: {
-      ...paramsTemplate.body,
+      mode: {
+        force: true
+      },
+      body: {
+        ...paramsTemplate.body,
 
-      variables: {
-        text: "Some text"
+        variables: {
+          text: "Some text"
+        }
       }
     }
+
+    const expected = {
+      ...fd.headers,
+
+      "content-length": await fd.getComputedLength(),
+      accept: "*/*"
+    }
+
+    const actual = await mockedMakePayload(params)
+
+    t.true(actual.body instanceof Readable)
+    t.is(fd.stream, actual.body)
+    t.deepEqual(expected, actual.headers)
   }
+)
 
-  const actual = mockedMakePayload(params)
+test(
+  "Always returns form-data payload strict mode as well as in force",
+  async t => {
+    const fd = new FormData()
+    const mockedMakePayload = pq("../../lib/util/makeFetcherPayload", {
+      "@octetstream/object-to-form-data": () => fd
+    })
 
-  t.true(actual.body instanceof Readable)
-  t.is(fd.stream, actual.body)
-  t.deepEqual({...fd.headers, accept: "*/*"}, actual.headers)
-})
+    const params = {
+      ...paramsTemplate,
 
-test("Always returns form-data payload strict mode as well as in force", t => {
-  const fd = new FormData()
-  const mockedMakePayload = pq("../../lib/util/makeFetcherPayload", {
-    "@octetstream/object-to-form-data": () => fd
-  })
+      mode: {
+        strict: true
+      },
+      body: {
+        ...paramsTemplate.body,
 
-  const params = {
-    ...paramsTemplate,
-
-    mode: {
-      strict: true
-    },
-    body: {
-      ...paramsTemplate.body,
-
-      variables: {
-        text: "Some text"
+        variables: {
+          text: "Some text"
+        }
       }
     }
+
+    const expected = {
+      ...fd.headers,
+
+      "content-length": await fd.getComputedLength(),
+      accept: "*/*"
+    }
+
+    const actual = await mockedMakePayload(params)
+
+    t.true(actual.body instanceof Readable)
+    t.is(fd.stream, actual.body)
+    t.deepEqual(expected, actual.headers)
   }
-
-  const actual = mockedMakePayload(params)
-
-  t.true(actual.body instanceof Readable)
-  t.is(fd.stream, actual.body)
-  t.deepEqual({...fd.headers, accept: "*/*"}, actual.headers)
-})
+)
