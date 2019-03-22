ee624d4eb62753c2cc22f80746e47f750d0739e6
Nick K <nick.kruchinin@gmail.com>
2019-03-23 01:41:33 +0300

    Add tests for makeFetcherPayload (for Node.js implementation)

---
+++ test/util/makeFetcherPayload.js
@@ -0,0 +1,86 @@
+const {createReadStream} = require("fs")
+const {Readable} = require("stream")
+
+const test = require("ava")
+const pq = require("proxyquire")
+const FormData = require("formdata-node").default
+
+const makePayload = require("../../lib/util/makeFetcherPayload")
+
+const paramsTemplate = {
+  mode: {},
+  body: {
+    operationName: undefined,
+    query: "",
+    variables: {}
+  },
+}
+
+function isJSONValid(string) {
+  try {
+    JSON.parse(string)
+
+    return true
+  } catch (_) {
+    return false
+  }
+}
+
+test("Returns a JSON payload request when request have no variables", t => {
+  const expected = {
+    body: JSON.stringify(paramsTemplate.body),
+    headers: {
+      "content-type": "application/json"
+    }
+  }
+
+  const actual = makePayload(paramsTemplate)
+
+  t.deepEqual(expected, actual)
+  t.true(isJSONValid(actual.body))
+})
+
+test("Returns a JSON payload when body does not contain files", t => {
+  const params = {
+    ...paramsTemplate,
+
+    body: {
+      ...paramsTemplate.body,
+
+      variables: {
+        text: "I beat Twilight Sparkle and all I got was this lousy t-shirt."
+      }
+    }
+  }
+
+  const actual = makePayload(params)
+
+  t.true(isJSONValid(actual.body))
+})
+
+test("Returns form-data payload whrn body have at least one file", t => {
+  const fd = new FormData()
+  const mockedMakePayload = pq("../../lib/util/makeFetcherPayload", {
+    "@octetstream/object-to-form-data": () => fd
+  })
+
+  const params = {
+    ...paramsTemplate,
+
+    body: {
+      ...paramsTemplate.body,
+
+      variables: {
+        file: createReadStream(__filename)
+      }
+    }
+  }
+
+  const actual = mockedMakePayload(params)
+
+  t.true(actual.body instanceof Readable)
+  t.is(fd.stream, actual.body)
+  t.deepEqual({...fd.headers, accept: "*/*"}, actual.headers)
+
+  t.pass()
+})
